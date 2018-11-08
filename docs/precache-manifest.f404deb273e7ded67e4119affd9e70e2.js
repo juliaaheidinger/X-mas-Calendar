b5d059952dc0a4ca8394660e7c04de82e9b7e47e
Julia Heidinger <juliaheidinger@gmx.net>
2018-11-08 17:07:59 +0100

    Add first version of calendar

---
+++ docs/precache-manifest.f404deb273e7ded67e4119affd9e70e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f875f7d077ab381f77d",
+    "url": "/static/js/main.1f875f7d.chunk.js"
+  },
+  {
+    "revision": "f4926859e068d12e2815",
+    "url": "/static/js/1.f4926859.chunk.js"
+  },
+  {
+    "revision": "1f875f7d077ab381f77d",
+    "url": "/static/css/main.2dc59434.chunk.css"
+  },
+  {
+    "revision": "faaff357ddbcfb4bcebb2b6f07a500a7",
+    "url": "/index.html"
+  }
+];
